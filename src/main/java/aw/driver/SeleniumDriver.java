e46ccb98d2adcb93c6d4ef9c1466a8e7637a4dce
qiangzhe6 <591158309@qq.com>
2019-07-16 18:04:52 +0800

    webui1.0

--- src/main/java/aw/driver/SeleniumDriver.java
+++ src/main/java/aw/driver/SeleniumDriver.java
@@ -1,7 +1,25 @@
 package aw.driver;
 
-public class SeleniumDriver {
-    public class SeleniumDriver{
+import autoframework.driver.logger.LoggerControler;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static autoframework.driver.logger.LoggerControler.getLogger;
+public class SeleniumDriver {
+    public static WebDriver;
+    public static WebDriver SeleniumDriver(String browser) {
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "D:\\testing\\webdriver_demo\\drivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equals("firefox")) {
+            System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+            driver = new FirefoxDriver();
+        } else {
+            log.severe("你输入的" + browser + "浏览器不支持打开");
+        }
+    }
+    public static void closed(){
+        driver.quit();
     }
 }
