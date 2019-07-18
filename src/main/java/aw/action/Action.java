0db83bbcc85d8a6c8f1a12f4141ad03c5df19bf5
qiangzhe6 <591158309@qq.com>
2019-07-18 20:17:52 +0800

    v1.0

---
+++ src/main/java/aw/action/Action.java
@@ -0,0 +1,38 @@
+package aw.driver.action;
+
+import aw.driver.elements.WebElementUtils;
+import aw.driver.logger.LoggerControler;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import java.util.ArrayList;
+import java.util.List;
+import static aw.driver.logger.LoggerControler.getLogger;
+
+/**
+ * Created by lenovo on 2016/11/8.
+ */
+public class Action {
+    final static LoggerControler log= getLogger(Action.class);
+    public static void click(By by,String text){
+        WebElementUtils.findElement(by).click();
+        log.info("点击："+by);
+    }
+    public static void sendkey(By by,String text){
+        WebElementUtils.findElement(by).sendKeys(text);
+        log.info("在"+by+"输入"+text);
+    }
+    public static String getText(By by){
+        String text=WebElementUtils.findElement(by).getText();
+        return text;
+    }
+    public static ArrayList getTexts(By by){
+        ArrayList arryList=new ArrayList();
+        List<WebElement> webElement=WebElementUtils.findElements(by);
+        for(int j=0;j<webElement.size();j++){
+            String text=webElement.get(j).getText();
+            arryList.add(text);
+
+        }
+        return arryList;
+    }
+}
