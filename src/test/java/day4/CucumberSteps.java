023edfcede4479729e58a6b232e92980b27e4423
John Doe <john@sample.com>
2019-08-06 09:17:00 +0800

    webui1.0

--- src/test/java/day4/CucumberSteps.java
+++ src/test/java/day4/CucumberSteps.java
@@ -1,36 +1,26 @@
 package day4;
 
-import aw.driver.SeleniumDriver;
-import aw.elements.WebElementUtils;
-import aw.tools.Suspend;
 import cucumber.api.java.zh_cn.假如;
 import cucumber.api.java.zh_cn.当;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import cucumber.api.java.zh_cn.那么;
 
 public class CucumberSteps {
-    WebDriver webDriver;
+
     @假如("^我打开\"(.*?)\"网站$")
     public void openWeb(String url){
-        webDriver= SeleniumDriver.SeleniumDriver("chrome");
-        Suspend.suspend(1000);
-        webDriver.get(url);
-    }
+        System.out.println("打开了"+url+"网站"+"\n");
+}
     @当("^我用\"(.*?)\"账号,用\"(.*?)\"密码$")
     public void login(String email,String pw){
-        WebElementUtils.findElement(By.id("lbNormal")).click();
-        Suspend.suspend(5000);
-        WebElement element=WebElementUtils.findElement(By.xpath("//*[@id=\"x-URS-iframe1564932773061.185\"]"));
-        webDriver.switchTo().frame(element);
-        WebElementUtils.findElement(By.xpath("//*[@id=\"auto-id-1564911566336\"]")).click();
-        WebElementUtils.findElement(By.xpath("//*[@id=\"auto-id-1564911566339\"]")).sendKeys("email");
-        WebElementUtils.findElement(By.xpath("//*[@id=\"auto-id-1564911566339\"]")).click();
-        WebElementUtils.findElement(By.xpath("//*[@id=\"auto-id-1564911566339\"]")).sendKeys("pw");
+        System.out.println("用账号"+email+"密码"+pw+"登陆"+"\n");
     }
     @当("^点击登录按钮$")
     public void submit(){
-        webDriver.findElement(By.id("dologin")).click();
+        System.out.println("点击登陆"+"\n");
+    }
+    @那么("^登录成功$")
+    public void sucess(){
+        System.out.println("登录成功"+"\n");
     }
 
 
